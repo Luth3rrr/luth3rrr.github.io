f51a980e1bc03f4afec6e2b0ccd611d3bff48333
Luth3rrr <92605914+Luth3rrr@users.noreply.github.com>
2023-12-03 15:30:09 +0200

    Create uv.config.js

---
+++ uv/uv.config.js
@@ -0,0 +1,13 @@
+// This file overwrites the stock UV config.js
+
+self.__uv$config = {
+  prefix: "/uv/service/",
+  bare: "/bare/",
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: "https://uv.signmyboat.gq/uv/uv.handler.js",
+  client: "https://uv.signmyboat.gq/uv/uv.client.js",
+  bundle: "https://uv.signmyboat.gq/uv/uv.bundle.js",
+  config: "https://uv.signmyboat.gq/uv/uv.config.js",
+  sw: "https://uv.signmyboat.gq/uv/uv.sw.js",
+};
